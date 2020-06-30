ce105029f75f3e5320fd77f5c5d95d974454d107
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-06-30 22:08:37 +0400

    Fix svg icons

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require("webpack");
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var extract_css = require("mini-css-extract-plugin");
+var SvgStorePlugin = require("external-svg-sprite-loader");
 
 module.exports = {
     mode: 'development',
@@ -16,17 +17,19 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env"
-                        ]
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env"
+                            ]
+                        }
                     }
-                }
+                ]
             },
             {
-                test: /\.(jpg|jpeg|png|gif|svg)?$/i,
+                test: /\.(jpg|jpeg|png|svg)?$/i,
                 use: [
                     'url-loader?limit=10000',
                     "img-loader"
@@ -59,14 +62,37 @@ module.exports = {
                     },
                 ] 
             },
+            {
+                test: /\.svg$/,
+                use: [
+                    SvgStorePlugin.loader
+                ]
+            }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({template: 'index.html'}),
-        new extract_css({
-            filename: "site.compressed.css",
-            chunkFilename: '[id].css',
-            publicPath: path.resolve(__dirname, '/dist/')
-        })
+        new HtmlWebpackPlugin(
+            {
+                template: 'index.html'
+            }
+        ),
+        new extract_css(
+            {
+                filename: "site.compressed.css",
+                chunkFilename: '[id].css',
+                publicPath: path.resolve(__dirname, '/dist/')
+            }
+        ),
+        new SvgStorePlugin(
+            {
+                sprite: {
+                    startX: 10,
+                    startY: 10,
+                    deltaX: 20,
+                    deltaY: 20,
+                    iconHeight: 20,
+                },
+            }
+        ),
     ]
 };
