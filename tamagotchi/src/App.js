e85c444264ff779ae13f7b612a8f0a6b1ed1c8bb
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-07-02 15:30:44 +0400

    ReactJS ("Hello, world!" commit)

---
+++ tamagotchi/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="game">
+        <span className="game-title">
+          MiniGame
+        </span>
+        <div className="game-field">
+          <div>Hello, world!</div>
+          <div>Hello, React!</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
