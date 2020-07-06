22139fb78d0504e3ab75810114dcff5e2e96d936
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-07-06 12:26:27 +0400

    Update controls' behaviour

--- tamagotchi/src/App.js
+++ tamagotchi/src/App.js
@@ -12,15 +12,20 @@ class App extends React.Component {
     fatigue: 75
   }
 
-  updateData = (value1, value2, value3) => {
-    this.setState({ hunger: value1})
-    this.setState({ health: value2})
-    this.setState({ fatigue: value3 })
+  updateData = (type, value, change) => {
+    for (let i = 0; i < type.length; i++) {
+      this.setState({ [type[i]]: value[i] - change[i] })
+    }
   }
 
   render () {
-  return (
-    <div className="App">
+
+    const hunger = this.state.hunger
+    const health = this.state.health
+    const fatigue = this.state.fatigue
+    const thirst = this.state.thirst
+
+    return (
       <div className="game">
         <span className="game-title">
           MiniGame
@@ -28,28 +33,35 @@ class App extends React.Component {
         <div className="game-field">
           <div className="wrapper">
             <div className="stats">
-              <Stat stat="Здоровье" value={this.state.health} />
-              <Stat stat="Жажда" value={this.state.thirst} />
-              <Stat stat="Голод" value={this.state.hunger} />
-              <Stat stat="Усталость" value={this.state.fatigue} />
+              <Stat stat="Здоровье" value={health} />
+              <Stat stat="Жажда" value={thirst} />
+              <Stat stat="Голод" value={hunger} />
+              <Stat stat="Усталость" value={fatigue} />
             </div>
             <div className="controls">
               <Button class="eat" text="ЕСТЬ"
-                      value1={this.state.hunger}
-                      value2={this.state.health}
-                      value3={this.state.fatigue}
-                      updateData={this.updateData} />
-              <Button class="drink" text="ПИТЬ" />
-              <Button class="sport" text="ЗАНЯТЬСЯ СПОРТОМ" />
-              <Button class="work" text="РАБОТАТЬ" />
+                      values={ [hunger, health, fatigue] }
+                      updateData={this.updateData}
+              />
+              <Button class="drink" text="ПИТЬ"
+                      values={ [thirst, health, fatigue] }
+                      updateData={this.updateData}
+              />
+              <Button class="sport" text="ЗАНЯТЬСЯ СПОРТОМ"
+                      values={ [health, hunger, thirst, fatigue] }
+                      updateData={this.updateData}
+              />
+              <Button class="work" text="РАБОТАТЬ"
+                      values={ [thirst, hunger, fatigue, health] }
+                      updateData={this.updateData}
+              />
             </div>
           </div>
           <div>Hello, React!</div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
--- tamagotchi/src/components/controls/Button.jsx
+++ tamagotchi/src/components/controls/Button.jsx
@@ -3,21 +3,33 @@ import './button.scss'
 
 function Button (props) {
 
-    const randomizer = (min, max) => {
+    const random = (min, max) => {
         const randInt = min + Math.random() * (max + 1 - min);
         return Math.floor(randInt)
     }
-    
-    const hp = randomizer(-2, 2)
 
-    const energy = randomizer(0, 5)
+    var type = []
+    var changes = []
+
+    if (props.class==="eat") {
+        type = ['hunger', 'health', 'fatigue']
+        changes = [10, random(-2, 2), random(5, 10)]
+    } else if (props.class==="drink") {
+        type = ['thirst', 'health', 'fatigue']
+        changes = [10, random(-1, 1), random(10, 15)]
+    } else if (props.class==="work") {
+        type = ['thirst', 'hunger', 'fatigue', 'health']
+        changes = [random(-40, -30), random(-20, -10), random(-35, -25), random(15, 25)]
+    } else if (props.class==="sport") {
+        type = ['health', 'hunger', 'thirst', 'fatigue']
+        changes = [random(-10, -1), -1*Math.floor(props.values[1]*0.1),
+                -1*Math.ceil(props.values[2]*0.08), random(-25, -20)]
+    }
 
     return (
         <button className={props.class}
                 type="button"
-                onClick={() => {props.updateData(props.value1 - 10,
-                                        props.value2 + hp,
-                                        props.value3 - energy);}}>
+                onClick={() => {props.updateData(type, props.values, changes);}}>
             {props.text}
         </button>
     )
