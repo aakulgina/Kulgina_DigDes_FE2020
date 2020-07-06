f39d96e0b075e872159c0088079c5c613fd76c10
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-07-06 15:56:30 +0400

    Add Input; change states, updateData(), Stats'

--- tamagotchi/src/App.js
+++ tamagotchi/src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 import './App.scss';
 import Button from './components/controls/Button.jsx'
 import Stat from './components/stats/stats.jsx'
+import Input from './components/input/input.jsx'
 
 class App extends React.Component {
 
   state = {
-    health: 75,
-    thirst: 75,
-    hunger: 75,
-    fatigue: 75
+    health: 50,
+    thirst: 50,
+    hunger: 50,
+    fatigue: 50
   }
 
   updateData = (type, value, change) => {
     for (let i = 0; i < type.length; i++) {
-      this.setState({ [type[i]]: value[i] - change[i] })
+      if (value[i] - change[i] <= 0) {
+        this.setState({ [type[i]]: 0 })  
+      } else if (value[i] - change[i] >= 100) {
+        this.setState({ [type[i]]: 100 })
+      } else {
+        this.setState({ [type[i]]: value[i] - change[i] })
+      }
     }
   }
 
@@ -33,10 +40,10 @@ class App extends React.Component {
         <div className="game-field">
           <div className="wrapper">
             <div className="stats">
-              <Stat stat="Здоровье" value={health} />
-              <Stat stat="Жажда" value={thirst} />
-              <Stat stat="Голод" value={hunger} />
-              <Stat stat="Усталость" value={fatigue} />
+              <Stat class="health" stat="Здоровье" value={health} />
+              <Stat class="thirst" stat="Жажда" value={thirst} />
+              <Stat class="hunger" stat="Голод" value={hunger} />
+              <Stat class="fatigue" stat="Усталость" value={fatigue} />
             </div>
             <div className="controls">
               <Button class="eat" text="ЕСТЬ"
@@ -57,7 +64,7 @@ class App extends React.Component {
               />
             </div>
           </div>
-          <div>Hello, React!</div>
+          <Input />
         </div>
       </div>
     );
