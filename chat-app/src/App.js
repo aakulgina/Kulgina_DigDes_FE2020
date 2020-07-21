35b0dc42f00a4506f963db4a8ce26e1e4bbe62a8
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-07-21 12:55:46 +0400

    React JS initial commit

---
+++ chat-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Hello, new React App!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
