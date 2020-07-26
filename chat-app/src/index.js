6b8091c6866fbf70146fdd125882c92357f417a8
Anastasiia Kulgina <a.a.kulgina@gmail.com>
2020-07-26 23:45:21 +0400

    Import Observer batching for ReactDOM

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import 'mobx-react-lite/batchingForReactDom'
 
 ReactDOM.render(
   (
